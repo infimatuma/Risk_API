e630e0ccbd928aa57003287064dfa66da73dce81
infimatuma <58609035+infimatuma@users.noreply.github.com>
2019-12-26 08:26:02 +0200

    Add files via upload
    started to implement bonus dices
    comments added

--- Risk_API/src/Battle.java
+++ Risk_API/src/Battle.java
@@ -1,21 +1,28 @@
 public class Battle {
     public static int resolve(int numberOfAttackers, int numberOfDefenders) {
-        while (numberOfAttackers > 0 && numberOfDefenders > 0) {
-            int attackRolls = Math.min(3, numberOfAttackers);
-            int defenceRolls = Math.min(2, numberOfDefenders);
-            Integer[] attack = Dice.roll(attackRolls);
-            Integer[] defence = Dice.roll(defenceRolls);
+        int attackBonusDice = 0; //to implement bonus
+        int defenceBonusDice = 0; //to implement bonus
+        while (numberOfAttackers > 0 && numberOfDefenders > 0) { //fight to death :)
+            int attackRolls = Math.min(3, numberOfAttackers) + attackBonusDice; //calculate amount of attacking dices
+            int defenceRolls = Math.min(2, numberOfDefenders) + defenceBonusDice; //calculate amount of attacking dices
+            Integer[] attack = Dice.roll(attackRolls); //roll attacker's dices and save sorted value (max -> min to array)
+            Integer[] defence = Dice.roll(defenceRolls); //roll defender's dices and save sorted value (max -> min to array)
             for(int counter = 0; counter < Math.min(attackRolls, defenceRolls); counter++) {
+                //compare maximum value of att/def dices
                 if (attack[counter] > defence[counter]) {
                     numberOfDefenders--;
+                    //attacker wins only if her dice value > defender's dice value
                 }
                 else {
                     numberOfAttackers--;
+                    //defender wins if her dice value > or = attacker's dice value
                 }
-            }
+            } //for
         System.out.println (numberOfAttackers + " vs. " + numberOfDefenders);
-        }
-        int result;
+        } //while
+        int result; //prepare result
+        //returns positive value of numberOfAttackers, if attacker wins;
+        // returns negative value of numberOfDefenders, if defender wins
         if(numberOfAttackers > numberOfDefenders) {
             result = numberOfAttackers;
         }
@@ -24,5 +31,5 @@ public class Battle {
         }
         System.out.println(result);
         return (result);
-    }
+    } //resolve method
 }
